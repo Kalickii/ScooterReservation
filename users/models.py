2da2a501b81bba631f3096c250b1209144eba5ad
Michał Kalicki <kalicki@Michas-MacBook-Air.local>
2024-11-12 13:06:57 +0100

    CustomUser fix

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext as _
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
@@ -21,11 +21,11 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
-    phone_number = PhoneNumberField(unique=False, blank=True, null=True)
+    phone_number = PhoneNumberField(unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -34,7 +34,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
