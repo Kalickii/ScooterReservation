f45351e69ccb2358ea36c53245cd36f61d04b49d
Michał Kalicki <kalicki@Michas-MacBook-Air.local>
2024-11-17 12:21:53 +0100

    Add ScooterSerializer

---
+++ scooters/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from scooters.models import Scooter
+
+
+class ScooterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Scooter
+        fields = [
+            'brand',
+            'scooter_model',
+            'capacity',
+            'year',
+            'registration_number',
+            'image',
+            'daily_price',
+            'weekly_price',
+            'monthly_price',
+            'deposit_amount',
+        ]
