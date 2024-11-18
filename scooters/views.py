3aa6c5f9a91d434553e3fde039b00931e5f8fc37
Michał Kalicki <kalicki@Michas-MacBook-Air.local>
2024-11-18 18:46:17 +0100

    Update ScooterListView - is_staff users see all scooters

--- scooters/views.py
+++ scooters/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from scooters.serializers import ScooterSerializer
 from .models import Scooter
@@ -8,5 +8,7 @@ class ScooterListView(ListAPIView):
     serializer_class = ScooterSerializer
 
     def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Scooter.objects.all()
         return Scooter.objects.filter(available=True)
-
