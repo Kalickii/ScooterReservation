ec834cfc11836af7496e898ba00be99d963ef766
Michał Kalicki <kalicki@Michas-MacBook-Air.local>
2024-11-20 19:30:04 +0100

    Improve base.html layout, add basic styles with css file

--- scooters/static/scooters/js/main.js
+++ scooters/static/scooters/js/main.js
@@ -8,6 +8,7 @@ fetch('http://127.0.0.1:8000/scooters/api/main/')
     })
     .then(data => {
         const ul = document.createElement('ul')
+        ul.classList.add('scooter')
         data.forEach(function (element){
             const li = document.createElement('li')
             li.innerHTML = `<a href="http://127.0.0.1:8000/scooters/main/${element.id}">${element.brand} ${element.scooter_model}</a>`
