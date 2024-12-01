f6aa0371da8e8f11adaea701d5e1b26d0759da52
Michał Kalicki <kalicki@Michas-MacBook-Air.local>
2024-12-01 14:11:34 +0100

    Add Reservation model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from scooters.models import Scooter
+from users.models import UserProfile
+
+
+class Reservation(models.Model):
+    userprofile_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    scooter_id = models.ForeignKey(Scooter, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    payment_status = models.BooleanField(default=False)
+    total_price = models.IntegerField(null=True, blank=True)
+
+    def calculate_price(self):
+        self.total_cost = self.scooter_id.deposit_amount
+        self.save()
